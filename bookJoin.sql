16daa555a8c996777231ca1f3437e605274a8b80
변의찬 <123133876+BEC1245@users.noreply.github.com>
2023-04-17 11:33:34 +0900

    bug fix

--- bookJoin.sql
+++ bookJoin.sql
@@ -1,22 +1,24 @@
 -- 1 마당서점의 고객이 요구하는 다음질문에 대해 SQL 문을 작성하시오.
 -- (5) 박지성이 구매한 도서의 출판사 수
-
-SELECT SUM(SALEPRICE)
-FROM BOOK B INNER JOIN ORDERS O
-USING(BOOKID)
-WHERE O.CUSTID = 1;
+select count(a.name)
+from customer a join orders b
+on a.custid = b.custid join book c
+on b.bookid = c.bookid
+where a.name = '박지성';
 
 -- (6) 박지성이 구매한 도서의이름, 가격, 정가, 판매가격의차이
 SELECT BOOKNAME, SALEPRICE, PRICE, PRICE - SALEPRICE
 FROM BOOK B INNER JOIN ORDERS O
-USING(BOOKID)
-WHERE O.CUSTID = 1;
+USING(BOOKID) JOIN CUSTOMER A
+USING (CUSTID)
+WHERE A.NAME = '박지성';
 
 -- (7) 박지성이구매하지않은도서의이름
 SELECT b.bookname
-FROM ORDERS O INNER JOIN BOOK B
-USING(BOOKID)
-where o.custid != 1;
+FROM BOOK B INNER JOIN ORDERS O
+USING(BOOKID) JOIN CUSTOMER A
+USING (CUSTID)
+where A.NAME != '박지성';
 
 -- 2 마당서점의 운영자와 경영자가 요구하는 다음 질문에 대해 SQL 문을작성하시오.
 
@@ -55,15 +57,17 @@ from customer d
 where d.custid in (select a.custid from orders a group by a.custid having avg(a.saleprice) > (select avg(c.saleprice) from orders c));
 
 -- (1) 박지성이 구매한 도서의 출판사와 같은 출판사에서 도서를 구매한 고객의 이름
-select *
-from customer cu join orders ord on cu.custid = ord.custid
-join book bk on ord.bookid = bk.bookid
+select CU.NAME
+from customer cu join orders ord 
+on cu.custid = ord.custid join book bk 
+on ord.bookid = bk.bookid
 where bk.publisher
-in(select publisher from customer a, orders b, book c where a.name = '박지성' and a.custid = b.custid and b.bookid = c.bookid) and cu.name != '박지성';
+in  (select publisher from customer a, orders b, book c where a.name = '박지성' and a.custid = b.custid and b.bookid = c.bookid) 
+    and cu.name != '박지성';
 
 -- 두 개 이상의 서로 다른 출판사에서 도서를 구매한 고객의 이름
 select cu.name 
 from customer cu
-where cu.custid in(select orders.custid from book natural join orders group by orders.custid having count(distinct publisher) >= 2);
+where cu.custid in(select custid from book natural join orders group by custid having count(distinct publisher) >= 2);
 
 
